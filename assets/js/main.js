9e9c4002f0e00598448f7d74cd01f3c0966d0d09
Pauline Andrzejewski <pauline.andrzejewski@gmail.com>
2020-11-16 15:30:25 +0100

    add a switch button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
-let button = document.querySelector("#button");
+let button1 = document.querySelector("#button1");
+let button2 =  document.querySelector("#button2");
 let paragraph = document.querySelector("#paragraph");
 
-button.addEventListener("click", () => {
+button1.addEventListener("click", () => {
     paragraph.classList.toggle("bg-white");
+
+    if (paragraph.classList.contains("bg-dark")) {
+        paragraph.classList.remove("bg-dark");
+    }
 });
 
+button2.addEventListener("click", () => {
+    if (paragraph.classList.contains("bg-white")) {
+        changeColor("bg-white", "bg-dark")
+    } else {
+        changeColor("bg-dark", "bg-white")
+    }
+    
+})
+
+function changeColor (remove, add) {
+    paragraph.classList.remove(remove);
+    paragraph.classList.add(add);
+}
